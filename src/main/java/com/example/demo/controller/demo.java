5d1f4f3c331e488f9e44e65cf03171c0ffec062a
15779073027 <1581928442@qq.com>
2020-07-16 17:12:22 +0800

    测试

--- src/main/java/com/example/demo/controller/demo.java
+++ src/main/java/com/example/demo/controller/demo.java
@@ -1,9 +1,13 @@
 package com.example.demo.controller;
 
+import org.springframework.http.converter.json.GsonBuilderUtils;
+
 /**
  * @Author liucheng
  * @Date 2020/7/16 16:30
  */
 public class demo {
-
+    public void test(){
+        System.out.println("hello");
+    }
 }
